905010b5974347eb5a1f1bbff616ff533954047f
Kenny Whyte <kawhyte@gmail.com>
2018-10-24 14:50:45 -0400

    Adding Callbacks

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 console.log('Before');
-let ans  = getUser(1);
-console.log(ans);
+
+getUser(1, getRepositories);
+
+function getRepositories(){
+    getRepositories(user.userName, getCommits);
+}
+
+function getCommits(repo){
+    getCommits(repo, displayCommits);
+}
+
+function displayCommits(commits){
+ console.log(commits);
+}
+
+
 console.log('After');
 
-
-
-
-function getUser (id){
+function getUser (id, callback){
 setTimeout(()=> {
+    console.log( 'Getting user info from DB...');
+    callback({ id: id , userName: 'Kenny'});
+    return; 
+    }, 2000);
+}
 
-console.log( 'Getting user info from DB...');
-
-return { id: id , userName: 'Kenny'}
-
-}, 2000);
+function getRepositories (username, callback) {
+    setTimeout(()=> {
+        console.log( 'calling...');
+        callback(['repo1', 'repo2', 'repo2']);
+    return;
+    }, 2000);
 
 }
